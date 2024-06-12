af8033cf69c0c780a21957865c744c2e5747038c
Finnedorn <daniele.giuntoli@outlook.com>
2024-06-12 14:53:28 +0200

    added task content page + bonus

--- src/assets/components/Footer/FooterComponent.jsx
+++ src/assets/components/Footer/FooterComponent.jsx
@@ -2,8 +2,13 @@ import footerStyle from "./Footer.module.css";
 
 const FooterComponent = () => {
   return (
-    <footer className={}>
-      
+    <footer className={footerStyle.footerBg}>
+      <p style={{
+        color: "white",
+        textAlign: "center"
+        }}>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum, nobis.
+      </p>
     </footer>
   );
 };
--- src/assets/components/Main/MainComponent.jsx
+++ src/assets/components/Main/MainComponent.jsx
@@ -3,12 +3,17 @@ import mainStyle from "./Main.module.css";
 
 const MainComponent = () => {
   return (
-    <main className={}>
-        <h1 className={}>
-            Il mio Blog
-        </h1>
+    <main className={mainStyle.mainWrapper}>
+      <h1 style={{
+        textAlign: "center",
+        padding: "30px 0px",
+        fontSize: "2.5rem"
+      }}>
+        Il mio Blog
+      </h1>
+      <div className={mainStyle.cardWrapper}>
         <CardComponent/>
-        
+      </div>
     </main>
   );
 };
